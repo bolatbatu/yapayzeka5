479fee6a4365998aa2c8c2908ce5af532a91862a
Batuhan Bolat <125748985+bolatbatu@users.noreply.github.com>
2023-06-25 00:30:31 +0300

    Update

--- Openai.py
+++ Openai.py
@@ -2,52 +2,42 @@ import openai
 import speech_recognition as sr
 import pyttsx3
 
-# OpenAI API anahtarınızı buraya girin
-openai.api_key = "YOUR_API_KEY"
+openai.api_key = "openai.api keyinizi giriniz."
 
-# Text-to-speech engine initialization
 engine = pyttsx3.init()
 
-# Define a function to speak the text
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-# Define a function to recognize the speech
 def recognize_speech():
-    # Initialize recognizer
     r = sr.Recognizer()
 
-    # Use the default microphone as the audio source
     with sr.Microphone() as source:
-        print("Listening...")
+        print("Dinliyorum...")
         r.pause_threshold = 1
         audio = r.listen(source)
 
     try:
-        # Convert audio to text using Google Speech Recognition
-        print("Recognizing...")
-        query = r.recognize_google(audio, language='en-US')
-        print(f"You said: {query}\n")
+        print("Kaydediliyor...")
+        query = r.recognize_google(audio, language='tr-TR')
+        print(f"Sorunuz: {query}\n")
 
     except sr.UnknownValueError:
-        # Handle unrecognized speech
-        print("I could not understand what you said.")
+        print("Dediğinizi anlamadım.")
         query = None
 
     return query
 
-# Define a function to execute the command
 def execute_command(command):
     response = openai.Completion.create(
-        engine="text-davinci-002",
+        engine="text-davinci-003",
         prompt=command,
         max_tokens=100
     )
 
     speak(response.choices[0].text)
 
-# Loop to continuously listen for commands
 while True:
     speak("What would you like me to do?")
     command = recognize_speech()
